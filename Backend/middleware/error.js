a26b7a4c7ba07f7261de16a301c0ec73b623c1f0
Surjesh Yadav <97423078+surjesh-yadav@users.noreply.github.com>
2022-08-02 05:44:24 +0530

    Update error.js

--- Backend/middleware/error.js
+++ Backend/middleware/error.js
@@ -1,6 +1,6 @@
 // const ErrorHandler = require("..utils/errorHandler");
 
-const ErrorHandler = require("../utils/errorHandler");
+const ErrorHandler = require("../utils/errorHandler");  
 
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
